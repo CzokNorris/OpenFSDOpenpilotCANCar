e2f13676bbbc0eacefc34e3d026332544633fc86
thomas <czoknorris@me.com>
2026-02-12 04:33:17 +0100

    sync

--- system/ui/lib/application.py
+++ system/ui/lib/application.py
@@ -641,9 +641,9 @@ class GuiApplication:
   def _set_styles(self):
     rl.gui_set_style(rl.GuiControl.DEFAULT, rl.GuiControlProperty.BORDER_WIDTH, 0)
     rl.gui_set_style(rl.GuiControl.DEFAULT, rl.GuiDefaultProperty.TEXT_SIZE, DEFAULT_TEXT_SIZE)
-    rl.gui_set_style(rl.GuiControl.DEFAULT, rl.GuiDefaultProperty.BACKGROUND_COLOR, _u32(rl.color_to_int(rl.BLACK)))
-    rl.gui_set_style(rl.GuiControl.DEFAULT, rl.GuiControlProperty.TEXT_COLOR_NORMAL, _u32(rl.color_to_int(DEFAULT_TEXT_COLOR)))
-    rl.gui_set_style(rl.GuiControl.DEFAULT, rl.GuiControlProperty.BASE_COLOR_NORMAL, _u32(rl.color_to_int(rl.Color(50, 50, 50, 255))))
+    rl.gui_set_style(rl.GuiControl.DEFAULT, rl.GuiDefaultProperty.BACKGROUND_COLOR, _s32(rl.color_to_int(rl.BLACK)))
+    rl.gui_set_style(rl.GuiControl.DEFAULT, rl.GuiControlProperty.TEXT_COLOR_NORMAL, _s32(rl.color_to_int(DEFAULT_TEXT_COLOR)))
+    rl.gui_set_style(rl.GuiControl.DEFAULT, rl.GuiControlProperty.BASE_COLOR_NORMAL, _s32(rl.color_to_int(rl.Color(50, 50, 50, 255))))
 
   def _patch_text_functions(self):
     # Wrap pyray text APIs to apply a global text size scale so our px sizes match Qt
